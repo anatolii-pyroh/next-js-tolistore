b7be3cddea1a86e1dbc4c8264e44f71e05173f65
AnatoliiCubex <anatolii.pyroh.cubex@gmail.com>
2023-04-26 13:21:54 +0300

    updated styles install Tailwind css styled header updated layout styles add colors,normalize css files

--- src/components/ui/header/header.component.tsx
+++ src/components/ui/header/header.component.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
-import styles from "./header.module.scss";
-
 import { useAuthSelector } from "@reducers/auth/useAuthSelector";
 
 export const HeaderComponent = () => {
@@ -14,12 +12,18 @@ export const HeaderComponent = () => {
   }, [accessToken]);
 
   return (
-    <header className={styles.header}>
+    <header className={"bg-light-primary p-4 px-20"}>
       {isLoggedIn ? (
         "Welcome, user!"
       ) : (
         <Link href='/login'>
-          <button>Log in</button>
+          <button
+            className={`
+            bg-white text-light-primary p-1.5 rounded
+            hover:bg-light-primary hover:text-white border-2 border-solid`}
+          >
+            Login
+          </button>
         </Link>
       )}
     </header>
